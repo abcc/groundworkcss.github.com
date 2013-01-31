858b74fa0b41ebc84d1e516d40b7ee460e9f731b
Gary Hepting <engineering+gary@sidereel.com>
2013-01-30 18:46:59 -0800

    created include files for templates

---
+++ _head.php
@@ -0,0 +1,11 @@
+  <!-- Modernizr -->
+  <script src="js/libs/modernizr-2.6.2.min.js"></script>
+  <!-- jQuery -->
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script type="text/javascript">window.jQuery || document.write('<script src="./js/libs/jquery-1.8.2.min.js"><\/script>')</script>
+  <!-- Groundwork CSS -->
+  <link type="text/css" rel="stylesheet" href="./css/groundwork.css">
+  <!-- Groundy theme files -->
+  <link type="text/css" rel="stylesheet" href="./css/groundy.css">
+</head>
+<body>
---
+++ _header.php
@@ -0,0 +1,40 @@
+  <header class="white band padded">
+    <div class="container">
+      <div class="row">
+        <div class="one third">
+          <h1 class="big responsive heading" data-scale="7.75" data-min="20" data-max="100"><a href="./"><span class="logo"></span> Groundwork<sup>Beta</sup></a></h1>
+        </div>
+        <div class="two thirds double pad-top">
+          <nav class="small-tablet">
+              <ul class="row">
+                  <li class="one small-tablet half"><a class="active" href="./">Home</a></li
+                  ><li class="one small-tablet half"><a href="./components.html">Components <i class="icon-caret-down"></i></a>
+                    <ul>
+                      <li><a href="./grid.html">Grid</a></li>
+                      <li><a href="./media-queries.html">Media Queries</a></li>
+                      <li><a href="./typography.html">Typography</a></li>
+                      <li><a href="./buttons.html">Buttons</a></li>
+                      <li><a href="./forms.html">Forms</a></li>
+                      <li><a href="./navigation.html">Navigation</a></li>
+                      <li><a href="./tabs.html">Tabs</a></li>
+                      <li><a href="./ui-elements.html">UI Elements</a></li>
+                      <li><a href="./icons.html">Icons</a></li>
+                      <li><a href="./responsiveText-js.html">ResponsiveText</a></li>
+                      <li><a href="./popovers-js.html">Popovers</a></li>
+                      <li><a href="./tooltips-js.html">Tooltips</a></li>
+                      <li><a href="./orbit-js.html">Orbit</a></li>
+                      <li><a href="./colorbox-js.html">ColorBox</a></li>
+                    </ul>
+                  </li>
+
+                  <!-- <li class="one small-tablet sixth"><a href="./layout-1.html">Layout 1</a></li
+                  ><li class="one small-tablet sixth"><a href="./layout-2.html">Layout 2</a></li
+                  ><li class="one small-tablet sixth"><a href="./layout-3.html">Layout 3</a></li
+                  ><li class="one small-tablet sixth"><a href="./layout-4.html">Layout 4</a></li
+                  ><li class="one small-tablet sixth"><a href="./layout-5.html">Layout 5</a></li> -->
+              </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
