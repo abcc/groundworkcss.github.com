76f34f3fc6399af71a474cd4dd4c73f6506692e0
Gary Hepting <engineering+gary@sidereel.com>
2013-01-31 09:04:30 -0800

    using Codekit includes

--- tooltips-js.php
+++
@@ -1,6 +0,0 @@
-<?php require_once("_doctype.php"); ?>
-  <title>Groundwork CSS &hearts;</title>
-<?php require_once("_head.php"); ?>
-<?php require_once("_header.php"); ?>
-
-<?php require_once("_scripts.php"); ?>
